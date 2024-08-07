395b08f2b93b04bae95ca29589ae1f378af28e80
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-08-07 08:04:52 +0000

    completed part of chapter 2. Continue on `Handling Multiple Command-Line Options`

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -12,7 +12,7 @@ func main() {
 	// Define a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
 
-	bytes := flag.Bool("b",false, "Count bytes")
+	bytes := flag.Bool("b", false, "Count bytes")
 	// Parsing the flags provided by the user
 	flag.Parse()
 
@@ -28,8 +28,9 @@ func count(r io.Reader, countLines, countBytes bool) int {
 	// If the count lines flag is not set, we want to count words so we define the scanner split type to words (default is split by lines)
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
-	}
+	}/
 
+	// If the count bytes flag is set, we want to count bytes so we define the scanner split type to bytes (default is split by lines)
 	if countBytes {
 		scanner.Split(bufio.ScanBytes)
 	}
