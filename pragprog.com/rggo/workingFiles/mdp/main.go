c27d8143e345ca6055d338a22c172274061d4ad0
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-08-11 21:10:39 -0400

    Completed chapter 4.

--- pragprog.com/rggo/workingFiles/mdp/main.go
+++ pragprog.com/rggo/workingFiles/mdp/main.go
@@ -1,81 +1,188 @@
 package main
 
 import (
-  "bytes"
-  "flag"
-  "fmt"
-  "os"
-  "path/filepath"
+	"bytes"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
 
-  "github.com/microcosm-cc/bluemonday"
-  "github.com/russross/blackfriday/v2"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
-  header = `<!DOCTYPE html>
+	defaultTemplate = `<!DOCTYPE html>
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Markdown Preview Tool</title>
+    <title>{{ .Title }}</title>
   </head>
   <body>
-`
-  footer = `
+<p><strong>File:</strong> {{ .FileName }}</p>
+{{ .Body }}
   </body>
 </html>
 `
 )
 
+type content struct {
+	Title    string
+	Body     template.HTML
+	FileName string
+}
+
 func main() {
-  // Parse flags
-  filename := flag.String("file", "", "Markdown file to preview")
-  flag.Parse()
+	// Parse flags
+	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
+	tFname := flag.String("t", "", "Alternate template name")
+	flag.Parse()
 
-  // If user did not provide input file, show usage
-  if *filename == "" {
-    flag.Usage()
-    os.Exit(1)
-  }
+	if err := run(*filename, *tFname, os.Stdin, os.Stdout, *skipPreview); err != nil {
 
-  if err := run(*filename); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+		os.Exit(1)
+	}
 }
 
-func run(filename string) error {
-  // Read all the data from the input file and check for errors
-  input, err := os.ReadFile(filename)
-  if err != nil {
-    return err
-  }
+func run(filename, tFname string, r io.Reader, out io.Writer, skipPreview bool) error {
+	// Check if an environment variable is set for the template
+	if tFname == "" {
+		tFname = os.Getenv("MDP_TEMPLATE")
+	}
 
-  htmlData := parseContent(input)
+	// Read all the data from the input file and check for errors
+	var input []byte
+	var err error
 
-  outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-  fmt.Println(outName)
+	// If a filename is provided, read from the file
+	if filename != "" {
+		input, err = os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+	} else {
+		// Otherwise, read from STDIN
+		input, err = io.ReadAll(r)
+		if err != nil {
+			return err
+		}
+	}
 
-  return saveHTML(outName, htmlData)
+	// Parse the content
+	htmlData, err := parseContent(input, tFname, filename)
+	if err != nil {
+		return err
+	}
+
+	// Create temporary file and check for errors
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+
+	// Close the temp file after creation
+	if err := temp.Close(); err != nil {
+		return err
+	}
+	outName := temp.Name()
+	fmt.Fprintln(out, outName)
+
+	// Save the generated HTML to the temporary file
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	// Ensure to delete the `Outname` file after the run function has completed
+	defer os.Remove(outName)
+
+	// Preview the generated HTML
+	return preview(outName)
 }
 
-func parseContent(input []byte) []byte {
-  // Parse the markdown file through blackfriday and bluemonday
-  // to generate a valid and safe HTML
-  output := blackfriday.Run(input)
-  body := bluemonday.UGCPolicy().SanitizeBytes(output)
+func parseContent(input []byte, tFname, fileName string) ([]byte, error) {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
 
-  // Create a buffer of bytes to write to file
-  var buffer bytes.Buffer
+	t, err := template.New("mdp").Parse(defaultTemplate)
+	if err != nil {
+		return nil, err
+	}
 
-  // Write html to bytes buffer
-  buffer.WriteString(header)
-  buffer.Write(body)
-  buffer.WriteString(footer)
+	// If user provided alternate template file, replace template
+	if tFname != "" {
+		t, err = template.ParseFiles(tFname)
 
-  return buffer.Bytes()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	fmt.Printf("File is: %s", fileName)
+	// Instantiate the content type, adding the title and body
+	c := content{
+		Title:    "Markdown Preview Tool",
+		Body:     template.HTML(body),
+		FileName: fileName,
+	}
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+
+	// Execute the template with the content type
+	if err := t.Execute(&buffer, c); err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
 }
 
 func saveHTML(outFname string, data []byte) error {
-  // Write the bytes to the file
-  return os.WriteFile(outFname, data, 0644)
+	// Write the bytes to the file
+	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	// Define executable based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	// Append filename to parameters slice
+	cParams = append(cParams, fname)
+
+	// Locate executable in PATH
+	cPath, err := exec.LookPath(cName)
+
+	if err != nil {
+		return err
+	}
+
+	// Open the file using default program
+	err = exec.Command(cPath, cParams...).Run()
+
+	// Give the browser some time to open the file before deleting it
+	time.Sleep(2 * time.Second)
+	return err
 }
--- pragprog.com/rggo/workingFiles/mdp/main_test.go
+++ pragprog.com/rggo/workingFiles/mdp/main_test.go
@@ -1,57 +1,71 @@
 package main
 
 import (
-  "bytes"
-  "os"
-  "testing"
+	"bytes"
+	"os"
+
+	"strings"
+	"testing"
 )
 
 const (
-  inputFile  = "./testdata/test1.md"
-  resultFile = "test1.md.html"
-  goldenFile = "./testdata/test1.md.html"
+	inputFile  = "./testdata/test1.md"
+	goldenFile = "./testdata/test1.md.html"
 )
 
 func TestParseContent(t *testing.T) {
-  input, err := os.ReadFile(inputFile)
-  if err != nil {
-    t.Fatal(err)
-  }
+	input, err := os.ReadFile(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  result := parseContent(input)
+	result, err := parseContent(input, "", inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  expected, err := os.ReadFile(goldenFile)
-  if err != nil {
-    t.Fatal(err)
-  }
+	expected, err := os.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  if !bytes.Equal(expected, result) {
-    t.Logf("golden:\n%s\n", expected)
-    t.Logf("result:\n%s\n", result)
-    t.Error("Result content does not match golden file")
-  }
+	if !bytes.Equal(expected, result) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
 }
 
 func TestRun(t *testing.T) {
-  if err := run(inputFile); err != nil {
-    t.Fatal(err)
-  }
+	var mockStdOut bytes.Buffer
 
-  result, err := os.ReadFile(resultFile)
-  if err != nil {
-    t.Fatal(err)
-  }
+	// Run the 'run' function with the test markdown file
+	if err := run(inputFile, "", os.Stdin, &mockStdOut, true); err != nil {
+		t.Fatal(err)
+	}
 
-  expected, err := os.ReadFile(goldenFile)
-  if err != nil {
-    t.Fatal(err)
-  }
+	// Get the name of the output HTML file from the buffer
+	resultFile := strings.TrimSpace(mockStdOut.String())
 
-  if !bytes.Equal(expected, result) {
-    t.Logf("golden:\n%s\n", expected)
-    t.Logf("result:\n%s\n", result)
-    t.Error("Result content does not match golden file")
-  }
+	// Read the generated HTML file
+	result, err := os.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  os.Remove(resultFile)
+	// Read the expected HTML output (golden file)
+	expected, err := os.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Compare the generated HTML to the expected HTML
+	if !bytes.Equal(expected, result) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+
+	// Clean up: remove the temporary file created by the run function
+	os.Remove(resultFile)
 }
